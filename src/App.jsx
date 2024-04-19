b1bb84b5fbce2da6fabece506d29ce67ef027eef
Alex <alexbacirea@gmail.com>
2024-04-19 12:52:04 +0300

    Adds header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,22 @@ import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { useGSAP } from '@gsap/react';
 
 import Layers from './components/layers';
+import Header from './components/header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
 
 function App() {
   return (
-    <>
-      <Layers />
-    </>
+    <BrowserRouter>
+      <main>
+        <Header />
+        <Routes>
+          <Route path="/" element={<h1>...</h1>} />
+          <Route path="/layers" element={<Layers />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
